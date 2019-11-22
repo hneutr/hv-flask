d8e647c5765cd079dd9b171c81a288fb19891d51
Hunter Wapman <hunter.wapman@gmail.com>
2019-11-21 18:41:50 -0700

    update

--- run.py
+++ run.py
@@ -60,7 +60,7 @@ freezer = Freezer(app)
 
 config_path = os.path.join(os.getcwd(), 'config.yaml')
 with open(config_path) as f:
-    config = yaml.load(f)
+    config = yaml.load(f, Loader=yaml.FullLoader)
 
 #-------------------------------------------------------------------------------
 # Pagination
